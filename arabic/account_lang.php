5fdf58cb2d98dee4728cf5e6c1c3a9dc17bd4009
merzak7 <merzak17@gmail.com>
2015-05-22 21:11:06 +0100

    Create & update account_lang.php

---
+++ arabic/account_lang.php
@@ -0,0 +1,31 @@
+ <?php
+ $lang['account_emailaddr'] 		= "البريد الالكتروني";
+ $lang['account_showemail'] 		= "اظهر بريدي الالكتروني علي صفحتي الخاصة";
+ $lang['account_firstname'] 		= "الاسم الاول";
+ $lang['account_lastname'] 		= "الاسم الاخير";
+ $lang['account_name'] 		    = "الاسم";
+ $lang['account_ircnick'] 		= "اسم IRC";
+ $lang['account_nick'] 			= "اسم الدخول";
+ $lang['account_newpasswd'] 		= "كلمة السر";
+ $lang['account_desiredpasswd'] 	= "كلمة السر الجديدة";
+ $lang['account_desirednick'] 	= "اسم الدخول الجديد";
+ $lang['account_passwdagain'] 	= "تاكيد كلمة السر";
+ $lang['account_location'] 		= "الموقع";
+ $lang['account_sitelang'] 		= "لغة الموقع";
+ $lang['account_chooseavatar'] 	= "اختيار صورة";
+ $lang['account_settings'] 		= "اعدادات الحساب";
+ 
+ $lang['account_forgotpasswd'] 	= 'نسيت كلمة السر';
+ $lang['account_join']		  	= "انضم الي %d مستخدم مثلك"; //%d in the number of users
+ 
+ $lang['account_already_got'] 	= 'لديك حساب ؟';
+ 
+ $lang['account_no_acc']			= "لا تملك حساب ؟";
+ 
+ //activate page
+ $lang['account_resend']			= "إعادة ارسال";
+ $lang['account_no_activate']	= "لم يصلك البريد";
+ $lang['account_verify']			= "تاكيد";
+ $lang['account_verify_code']	= "رمز التاكيد";
+ $lang['account_please_verify']	= "فضلا تفقد بريدك الالكتروني للحصول علي رمز التفعيل وادخله هنا";
+ $lang['account_verify_sent']	= "تم ارسال التاكيد الي %s"; // %s is the email address, please always include it
