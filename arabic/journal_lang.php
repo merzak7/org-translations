f146a0b7024d7db6f3de6655a69646c5bad62f06
merzak7 <merzak17@gmail.com>
2015-05-23 17:29:46 +0100

    Update journal_lang.php

---
+++ arabic/journal_lang.php
@@ -0,0 +1,6 @@
+<?php
+$lang['journal_loadmore']				= "جلب المزيد";
+$lang['journal_newsandupdates']			= "الاخبار والتحديثات";
+$lang['journal_noarticles']				= "لم يتم العثور على مقالات.";
+$lang['journal_by']						= "بواسطة";
+$lang['journal_readmore']				= "قراءة المزيد";
