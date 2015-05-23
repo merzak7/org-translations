347b508391c80785385bc6b52569d86453a79c69
merzak7 <merzak17@gmail.com>
2015-05-23 18:18:15 +0100

    Update main_lang.php

--- arabic/main_lang.php
+++ arabic/main_lang.php
@@ -1,89 +1,88 @@
 <?php
 //Menu/Page items
-$lang['global_home']            = "Home";
-$lang['global_discover']        = "Discover";
-$lang['global_store']           = "Store";
-$lang['global_journal']         = "Journal";
-$lang['global_support']         = "Support";
-$lang['global_developer']       = "Developer";
-$lang['global_getinvolved']     = "Get Involved";
-$lang['global_profile']         = "Profile";
-$lang['global_search']          = "Search";
-$lang['global_docs']            = "Docs";
-$lang['global_settings']        = "Settings";
-$lang['global_donate']        	= "Donate";
-$lang['global_code of conduct'] = "Code Of Conduct";
+$lang['global_home']            = "الرئيسية";
+$lang['global_discover']        = "اكتشف";
+$lang['global_store']           = "المتجر";
+$lang['global_journal']         = "الصحافة";
+$lang['global_support']         = "الدعم";
+$lang['global_developer']       = "التطوير";
+$lang['global_getinvolved']     = "انضم";
+$lang['global_profile']         = "الملف الشخصي";
+$lang['global_search']          = "بحث";
+$lang['global_docs']            = "المستندات";
+$lang['global_settings']        = "الاعدادات";
+$lang['global_donate']        	= "تبرع";
+$lang['global_code of conduct'] = "ضوابط السلوك";
 
 //Footer
-$lang['global_changelang']      = "Change Site Language";
-$lang['global_content_license'] = "Content licensed under:";
-$lang['global_team']            = "The Teams";
-$lang['global_terms']           = "Terms of Use";
-$lang['global_user_guide']      = "User Guide";
-$lang['global_answers']         = "Answers";
-$lang['global_user_chat']       = "User Chat";
-$lang['global_dev_chat']        = "Developer Chat";
-$lang['global_design']          = "Design";
-$lang['global_code']            = "Code";
-$lang['global_manage']          = "Manage";
-$lang['global_distribute']      = "Distribute";
+$lang['global_changelang']      = "تغير لغة الموقع";
+$lang['global_content_license'] = "المحتوي تحت رخصة:";
+$lang['global_team']            = "الشروط";
+$lang['global_terms']           = "شروط الاستخدام";
+$lang['global_user_guide']      = "دليل المستخدم";
+$lang['global_answers']         = "الاجابات";
+$lang['global_user_chat']       = "دردشة المستخدمين";
+$lang['global_dev_chat']        = "دردشة المطورين";
+$lang['global_design']          = "التصميم";
+$lang['global_code']            = "البرمجة";
+$lang['global_manage']          = "الادارة";
+$lang['global_distribute']      = "التوزيع";
 
-$lang['global_news']            = 'News';
-$lang['global_dev']             = 'Developer';
-$lang['global_edtrl']           = 'Editorial';
-$lang['global_intv']            = 'Interview';
-$lang['global_mini']            = 'Mini';
-$lang['global_ux']              = 'User Experience';
-$lang['global_video']           = 'Video';
+$lang['global_news']            = 'الاخبار';
+$lang['global_dev']             = 'التطوير';
+$lang['global_edtrl']           = 'التحرير';
+$lang['global_intv']            = 'المقابلات';
+$lang['global_mini']            = 'مصغر';
+$lang['global_ux']              = 'تجربة المستخدم';
+$lang['global_video']           = 'الفيديو';
 
-$lang['global_team_web']        = 'Web';
-$lang['global_team_press']      = 'Press';
-$lang['global_team_ux']         = 'Design';
-$lang['global_team_mod']        = 'Moderator';
-$lang['global_team_dev']        = 'Developer';
-$lang['global_team_core']       = 'Council';
-
-$lang['global_login_header']    = "Just one second";
-$lang['global_login_text']      = "Login in to comment, ask questions and more.";
-$lang['global_forgot_password'] = "Forgot your password?";
-$lang['global_username']        = "Username";
-$lang['global_password']        = "Password";
-$lang['global_jsnotice']        = "Please enable JavaScript in your browser to experience elementaryos.org fully";
-$lang['global_comment']         = "Comment";
-$lang['global_comments']        = "Comments";
-$lang['global_view_profile']	= "View user profile.";
-$lang['global_load_more']		= "Load More";
+$lang['global_team_web']        = 'ويب';
+$lang['global_team_press']      = 'صحافة';
+$lang['global_team_ux']         = 'مصمم';
+$lang['global_team_mod']        = 'مشرف';
+$lang['global_team_dev']        = 'مطور';
+$lang['global_team_core']       = 'المجلس';
+$lang['global_login_header']    = "فقط ثانية واحدة";
+$lang['global_login_text']      = "ادخل لتعلق او تسأل او اي شئ";
+$lang['global_forgot_password'] = "نسيت كلمة السر؟";
+$lang['global_username']        = "اسم الدخول";
+$lang['global_password']        = "كلمة السر";
+$lang['global_jsnotice']        = "فضلا قم بتفعيل الجافاسكربت لتتمتع بمشاهدة الموقع كاملا";
+$lang['global_comment']         = "تعليق";
+$lang['global_comments']        = "تعليقات";
+$lang['global_view_profile']	= "شاهد ملف العضو";
+$lang['global_load_more']		= "تحميل المزيد";
 
 //actions
-$lang['global_login']           = "Login";
-$lang['global_logout']          = "Logout";
-$lang['global_register']        = "Register";
-$lang['global_status']	        = "Status";
-$lang['global_save']            = "Save";
-$lang['global_edit'] 			= "Edit";
-$lang['global_delete'] 			= "Delete";
-$lang['global_cancel']          = "Cancel";
-$lang['global_reply']           = "Reply";
-$lang['global_report']          = "Report";
+$lang['global_login']           = "دخول";
+$lang['global_logout']          = "خروج";
+$lang['global_register']        = "تسجيل";
+$lang['global_status']	        = "الحالة";
+$lang['global_save']            = "حفظ";
+$lang['global_edit'] 			= "تحرير";
+$lang['global_delete'] 			= "حذف";
+$lang['global_cancel']          = "الغاء";
+$lang['global_reply']           = "رد";
+$lang['global_report']          = "تبليغ";
 
 //Strings that don't need there own file, 
 // no need to put extra load on the system for one string
-$lang['global_email']           = 'Email';
-$lang['write_comment']          = 'Write a Comment&#8230;';
-$lang['search_results']         = 'Search Results';
-$lang['error_gohome']           = 'Go to the Home Page';
-$lang['error_404_title']        = 'The page you’re looking for can’t be found';
-$lang['error_404_descr']        = "There isn’t anything located at \"%s\". Check the web address for mispelled words and try again."; //%s = page url without domain
-$lang['error_403_title']        = 'You don’t have permission to view this page';
-$lang['error_403_descr']        = 'Access to this page is restricted to authorised users only.';
-$lang['error_500_title']        = 'There was a problem opening the web page';
-$lang['error_500_descr']        = 'Something went wrong while trying to access the webpage. Please try again later.';
-$lang['forgot_password_title']  = 'Forgot your password?';
-$lang['forgot_password_sub']    = 'It happens to the best of us.';
-$lang['forgot_password_req']    = 'Request new password';
-$lang['password_reset']         = "Your password has been reset";
+$lang['global_email']           = 'البريد الالكتروني';
+$lang['write_comment']          = 'اكتب تعليق&#8230;';
+$lang['search_results']         = 'نتائج البحث';
+$lang['error_gohome']           = 'اذهب الي الصفحة الرئيسية';
+$lang['error_404_title']        = 'الصفحة التي تبحث عنها غير موجودة';
+$lang['error_404_descr']        = "لا يوجد اي شئ هنا \"%s\". راجع العنوان، ربما توجد كلمة خطا وحاول مرة اخري"; //%s = page url without domain
+$lang['error_403_title']        = 'ليس لديك صلاحيات لدخول هذه الصفحة';
+$lang['error_403_descr']        = 'يقتصر الدخول الي هذه الصفحة للاعضاء المخول لهم بالدخول فقط';
+$lang['error_500_title']        = 'هنالك مشكلة حدثت عند فتح الصفحة';
+$lang['error_500_descr']        = 'لقد حدث خطأ ما عند فتح الصفحة، فضلا حاول مرة اخرى';
+$lang['forgot_password_title']  = 'نسيت كلمة السر ؟';
+$lang['forgot_password_sub']    = 'تحدث معنا جميعا';
+$lang['forgot_password_req']    = 'طلب كلمة سر جديدة';
+$lang['password_reset']         = "تم اعادة تعين كلمة السر الخاصة بك";
 
 
-$lang['comment_none_found']     = "This %s has no comments yet."; //%s = type. Anwser, Article, Question
-$lang['comment_login']     		= "<a href='%s'>Login</a> to place a comment."; //%s = full url to login page
-$lang['comment_banned']    		= "You have been banned, you can not place comments.";
+$lang['comment_none_found']     = "هله %s لا تحتوى علي تعليقات"; //%s = type. Anwser, Article, Question
+$lang['comment_login']     		= "قم <a href='%s'>بالدخول</a> لاضافة تعليق"; //%s = full url to login page
+$lang['comment_banned']    		= "لقد تم حظرك لذلك لا تستطيع التعليق";
